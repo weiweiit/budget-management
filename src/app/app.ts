b96807945a0b0ed489ff24177f80d95cc565d140
weiweiit <tweissem@gmail.com>
2025-07-24 12:26:40 +0200

    project configuration : eslint, prettier, primeng

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('budget-management');
